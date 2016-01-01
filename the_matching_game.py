226391e2e69552a2de6506a735c0ac8ef5ce6965
Isabel Lee <ijl0322@gmail.com>
2016-01-01 17:10:23 +0800

    showBoard added

--- the_matching_game.py
+++ the_matching_game.py
@@ -1,26 +1,31 @@
 import random
 
-board = []
-for i in range(4):
-    board += [["O"]*4]
-
-
-def showboard(board):
+def showBoard(board):
     print "-"*17
     for line in board:
         board_line = "|"
         for item in line:
-            board_line += " " + item[0] + " " + "|"
+            if item[1]: 
+                board_line += " " + item[0] + " " + "|"
+            else:
+                board_line += " " + " " + " " + "|"
         print "%s\n" %board_line, "-"*17
             
-def init_board(board):
+def init_board():
+    board = []
+    for i in range(4):
+        board += [["O"]*4]
+
     item_list = [("A",False),("B",False),("C",False),("D",False),("E",False),\
     ("F",False),("G",False),("H",False),("A",False),("B",False),("C",False),("D",False),("E",False),\
     ("F",False),("G",False),("H",False)]
+    
     for i in range(4):
         for j in range(4):
             item = item_list.pop(random.randrange(len(item_list)))
             board[i][j] = item
-init_board(board)
-showboard(board)       
+    return board
+board = init_board()
+
+showBoard(board)       
     
